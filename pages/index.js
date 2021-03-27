e104b39d82271a898e6b5b30c1b31c6fcaab557e
FortsetzungF <fortsetzungf@gmail.com>
2021-03-27 17:34:40 +0100

    add faecher index page

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,17 @@ export default function Home() {
   return (
     <div>
       <Layout home>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
-        dolore quo sint officiis, reiciendis, quibusdam voluptas error iste
-        animi numquam quasi quam architecto, iusto modi! Minus consequatur nulla
-        repudiandae illo iusto tenetur voluptates doloremque quae. At doloremque
-        error veritatis recusandae vitae reiciendis exercitationem ut
-        perspiciatis cupiditate, cum officia odit animi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic cupiditate
+        itaque tempora laborum quas expedita, accusantium alias adipisci natus
+        quod, error tempore! Quasi, ex esse architecto exercitationem nulla iure
+        nihil sed officiis repudiandae! Culpa autem deleniti voluptas maiores.
+        Doloribus nostrum suscipit placeat, repudiandae consectetur praesentium
+        debitis et harum eligendi blanditiis iusto illo! Quo quas cumque esse
+        nostrum maiores eaque sapiente, eveniet culpa nemo nihil amet doloremque
+        neque hic suscipit! Iure dignissimos maxime assumenda est doloribus,
+        ipsa corporis. Itaque veniam pariatur ratione vitae natus voluptatibus
+        libero consequatur, voluptatem a recusandae, quisquam deleniti quos
+        error earum nulla esse. Aut iusto quae distinctio.
       </Layout>
     </div>
   );
