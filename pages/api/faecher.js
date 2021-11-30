78b85864207f504622e1719b3359e6e228c3eba5
FortsetzungF <fortsetzungf@gmail.com>
2021-11-30 21:31:00 +0100

    add lernzettel category

--- pages/api/faecher.js
+++ pages/api/faecher.js
@@ -1,3 +1,3 @@
 export default (req, res) => {
-    res.status(200).json({ faecher: ["Deutsch", "Mathe", "Englisch", "Französisch", "Latein", "Spanisch", "Kunst", "Geschichte", "Musik", "PoWi", "Erdkunde", "Biologie", "Chemie", "Physik", "Informatik", "Philosophie", "Religion", "WeNo"] })
+    res.status(200).json({ faecher: ["Deutsch", "Mathe", "Englisch", "Französisch", "Latein", "Spanisch", "Kunst", "Geschichte", "Musik", "PoWi", "Erdkunde", "Biologie", "Chemie", "Physik", "Informatik", "Philosophie", "Religion", "WeNo", "Lernzettel"] })
 }
