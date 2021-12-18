9c468d65719edc95032deea1576fc0db831c31a9
FortsetzungF <fortsetzungf@gmail.com>
2021-12-18 15:25:16 +0100

    add frontpage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Layout from "@components/Layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Layout>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-      </Layout>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <div className="text-4xl lg:text-8xl font-bold uppercase">
+        schulmappe
+      </div>
+      <div className="lg:text-xl font-bold uppercase">
+        <Link href="/app">
+          <a className="hover:text-red-700 transition-colors duration-200">login</a>
+        </Link>
+      </div>
     </div>
   )
 }
