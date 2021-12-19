24600448ed8a81dca9b8523a4ef3d4d95a976f2f
FortsetzungF <fortsetzungf@gmail.com>
2021-12-19 12:48:00 +0100

    update Link component

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return(
     <div className="bg-gray-800">
       <h1 className="text-4xl lg:text-8xl font-extrabold italic uppercase">
-        <Link href="/app">schulmappe.</Link>
+        <Link href="/app" noUnderline>schulmappe.</Link>
       </h1>
 
       {/* <p className="font-mono italic text-xl ml-32">
--- components/Link.js
+++ components/Link.js
@@ -1,18 +1,26 @@
 import NextLink from "next/link"
 
-export default function Link({ children, href, external }) {
+export default function Link({ children, href, external, noUnderline }) {
 
   if (external) {
     return(
       <NextLink href={href}>
-        <a className="hover:text-teal-400 transition-colors duration-200 after:content-['_↗']">{children}</a>
+        <a className="hover:text-teal-400 transition-colors duration-200 underline underline-offset-4 decoration-teal-400 after:content-['_↗']">{children}</a>
+      </NextLink>
+    )
+  }
+
+  if (noUnderline) {
+    return(
+      <NextLink href={href}>
+        <a className="hover:text-teal-400 transition-colors duration-200">{children}</a>
       </NextLink>
     )
   }
 
   return(
     <NextLink href={href}>
-      <a className="hover:text-teal-400 transition-colors duration-200">{children}</a>
+      <a className="hover:text-teal-400 transition-colors duration-200 underline underline-offset-4 decoration-teal-400">{children}</a>
     </NextLink>
   )
 }
