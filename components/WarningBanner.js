40c92ba79f78a96515b4586201fc4fd6cc3dadda
FortsetzungF <fortsetzungf@gmail.com>
2021-12-20 20:15:17 +0100

    add WarningBanner component

---
+++ components/WarningBanner.js
@@ -0,0 +1,33 @@
+export default function WarningBanner({ children, warning, fatalError, everythingIsFine }) {
+
+  if (fatalError) {  
+    return(
+      <div className="bg-red-600 p-2 my-4"> 
+        <div className="font-bold uppercase text-xl lg:text-2xl text-center">
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+  if (warning) {  
+    return(
+      <div className="bg-orange-500 p-2 my-4 text-black"> 
+        <div className="font-bold uppercase text-xl lg:text-2xl text-center">
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+  if (everythingIsFine) {  
+    return(
+      <div className="bg-lime-500 p-2 my-4 text-black"> 
+        <div className="font-bold uppercase text-xl lg:text-2xl text-center">
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+}
