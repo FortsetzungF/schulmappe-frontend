1086761efbb2e474281797b60b381c3303ec53a6
FortsetzungF <fortsetzungf@gmail.com>
2022-01-04 17:56:15 +0100

    update copyright in footer

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
 	return (
 		<div className="relative h-8 2xl:h-5 text-center font-ubuntu font-thin text-gray-500 dark:text-gray-300">
 			<div className="absolute inset-x-0 bottom-2 uppercase">
-				&copy; 2021 Privatchat!
+				&copy; 2021 - 2022 Privatchat!
 
-				<span className="pl-2 normal-case">
+				<span className="pl-4 normal-case">
 					<Link href="https://github.com/FortsetzungF/schulmappe-frontend/issues">
 						<a className="hover:text-red-700 transition-colors duration-500">bugs?</a>
 					</Link>
